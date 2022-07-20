9d82daa300339d9903346656c6d69a135aa98926
PierTwo <105090994+PierTwo@users.noreply.github.com>
2022-07-19 21:14:09 -0500

    fetched the LoC API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,26 @@
 const select = document.getElementById("select");
 const submit = document.getElementById("submit");
 
-var apiUrl = "https://www.loc.gov/" + format + "/?q=" + "&fo=json";
+var locApi = "https://www.loc.gov/?fo=json"
+// var apiUrl = "https://www.loc.gov/" + format + "/?q=" + "&fo=json";
 
 select.addEventListener("change", function handleChange(event) {
-  event.preventDefault();
-  console.log(event.target.value);
-  formatType = event.target.value;
+    event.preventDefault();
+    console.log(event.target.value);
+    formatType = event.target.value;
 });
 
 submit.addEventListener("click", function handleClick(event) {
-  event.preventDefault();
-  console.log("clicked");
-  getSearchProps(formatType);
+    event.preventDefault();
+    console.log("clicked");
+    getSearchProps(formatType);
 });
+
+fetch(locApi)
+    .then(function (response) {
+        console.log(response.status)
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    });
